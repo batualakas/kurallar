61ac9822a2010982dc2724fa593d6c0e63110778
batualakas <batualakas@gmail.com>
2021-08-26 13:33:17 +0300

    Add project files.

---
+++ visualstudio3gun/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace visualstudio3gun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MANTIKSAL OPERATORLER DEVAMKE
+            // ÖRNEK dışarıdan alınan iki sayının toplamının karesini bulunuz 
+
+            //Console.WriteLine("Birinci sayı giriniz");
+            //var sayi1 = Console.ReadLine();
+            //int intsayi1 = Convert.ToInt32(sayi1);
+
+            //Console.WriteLine("İkinci Sayıyı giriniz");
+            //var sayi2 = Console.ReadLine();
+            //int intsayi2 = Convert.ToInt32(sayi2);
+
+            //var sonuc = (intsayi1 + intsayi2) * (intsayi2 + intsayi1);
+
+            //Console.WriteLine($"Sonuc:{sonuc}");
+
+            // Vize notunun %30' u final notunu %70 ini hesaplayarak  ortalamayı bulunuz.
+
+            //Console.WriteLine("vize notunuz");
+            //var vize = Console.ReadLine();
+            //double vizeNotu = Convert.ToDouble(vize);
+
+            //Console.WriteLine("final notunuz");
+            //var final = Console.ReadLine();
+            //double finalNotu = Convert.ToDouble(final);
+
+            //var toplamNot = (vizeNotu * 30 / 100 + finalNotu * 70 / 100);
+            //Console.WriteLine($"Notunuz: {toplamNot}");
+
+
+
+
+
+            // kullancıdan adını soyadını mail uzantısını alınız ve ad.soyad@uzanti.com mailini oluşturunuz ve ekranda gösteriniz.
+
+            //Console.WriteLine("Adınız?");
+            //var isim = Console.ReadLine();
+
+            //Console.WriteLine("Soyadınız?");
+            //var soyİsim = Console.ReadLine();
+
+            //Console.WriteLine("oluşturulmasını istediğiniz mail uzantısını '@' işareti olmadan yazınız Orn: gmail, hotmail");
+            //var mailAdresi = Console.ReadLine();
+
+            //var adSoyadMailadresi = (isim+ "."+soyİsim + "@"  + mailAdresi+ ".com");
+            //Console.WriteLine($"Adınız Soyadınız ve Mail adresiniz {adSoyadMailadresi }");
+                
+        }
+            string kral "BATU AMA \ " KRAL OLAN \ " ";
+    }
+}
